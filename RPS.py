b0c6dfecaf76d7910184bf83e9a906f6f7c136fa
LukeSwier <lswier@unomaha.edu>
2025-09-14 21:22:03 +0000

    Finished R,P,S

--- RPS.py
+++ RPS.py
@@ -8,14 +8,59 @@ def main():
   wins = 0
   ties = 0
   losses = 0
-  #Create a loop that continues as long as the user wants to play.
-  #User can play as many games as they wish.
 
-  #Randomly choose the computer between 'R', 'P', or 'S'
-  #Prompt the user for their RPS selection
-  #Determine winner and state what happened to the user
-  #Ask the user if they would like to play again.
+  playagain = "Y"
+  while playagain == "Y":
+    #Create a loop that continues as long as the user wants to play.
+    #User can play as many games as they wish.
+    computer = random.choice(["R","R","R","P","P","P","S","S"])
+    player = input("Choose an object (R, P, S): ")
+    #Randomly choose the computer between 'R', 'P', or 'S'
+    #Prompt the user for their RPS selection
+    if computer == "R":
+      print("Computer chose Rock")
+    elif computer == "P":
+      print("Computer chose Paper")
+    else:
+      print("Computer chose Scissors")
+      
+    if player == "R":
+      print("Player chose Rock")
+    elif player == "P":
+      print("Player chose Paper")
+    else:
+      print("Player chose Scissors")  
 
+    if player == "R" and computer == "R":
+      print("Tie")
+      ties = ties + 1
+    if player == "P" and computer == "P":
+      print("Tie")
+      ties = ties + 1
+    if player == "S" and computer == "S":
+      print("Tie")
+      ties = ties + 1
+    if player == "R" and computer == "P":
+      print("You lose :(")
+      losses = losses + 1
+    if player == "P" and computer == "S":
+      print("You lose :(")
+      losses = losses + 1
+    if player == "S" and computer == "R":
+      print("You lose :(")
+      losses = losses + 1
+    if player == "R" and computer == "S":
+      print("You win :)")
+      wins = wins + 1
+    if player == "P" and computer == "R":
+      print("You win :)")
+      wins = wins + 1
+    if player == "S" and computer == "P":
+      print("You win :)")
+      wins = wins + 1
+    #Determine winner and state what happened to the user
+    #Ask the user if they would like to play again.
+    playagain = input("Play again? (Y/N): ")
   #In the end, print the stats
   print("Wins \t Ties \t Losses")
   print("---- \t ---- \t ------")
